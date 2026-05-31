392c2dda481937bb459058a7c0abf17bc7c0f737
T-K-Aaron <2425403354@live.gctu.edu.gh>
2026-05-31 17:42:28 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in large letters.
+# Assignment Number: 1
+#
+# Name: Aaron Kwei Tetteh
+# STUDENT ID: 2425403354
+# Email: 2425403354@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Aaron Kwei Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print stylized initials AKT
+
+    print()
+    print("...AKT")
+    print()
+
+print("...AAAAAAAAAAAA........KK........KK........TTTTTTTTTTTT...")
+print("...AAAAAAAAAAAA........KK......KK..........TTTTTTTTTTTT...")
+print("...AA........AA........KK....KK.................TT........")
+print("...AA........AA........KK..KK...................TT........")
+print("...AAAAAAAAAAAA........KKKK.....................TT........")
+print("...AAAAAAAAAAAA........KKKK.....................TT........")
+print("...AA........AA....... KK..KK...................TT........")
+print("...AA........AA........KK....KK.................TT........")
+print("...AA........AA...**...KK......KK.....**........TT......**")
+print("...AA........AA...**...KK........KK...**........TT......**")
+
+print()
 
 
-
-main()
+main()  
